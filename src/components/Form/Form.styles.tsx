b29b4b1bb50f64560e39b2089ae485bd64662983
Alex Voievudko <oleksandrv@zeptolab.com>
2023-05-12 12:07:57 +0200

    [Contact Page] Added Form component

---
+++ src/components/Form/Form.styles.tsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+import { ContentTitle } from '../Typography/Typography'
+
+// -------------------------------------------------------------
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled(ContentTitle)`
+  margin-bottom: 1.4rem;
+`
+
+export const Subtitle = styled.p`
+  padding: 0;
+  margin: 0;
+  margin-bottom: 2rem;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2.4rem;
+`
+
+export const Input = styled.input<{ $error: boolean }>`
+  display: block;
+  width: 100%;
+  background-color: #161616;
+  border: none;
+  border-bottom: 1px solid ${({ $error, theme }) => ($error ? theme.colors.error : '#777777')};
+  border-radius: 0;
+  color: #777777;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.5;
+  height: 44px;
+  padding: 9px 14px;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    outline: none;
+    border: none;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
+  }
+`
+
+export const TextArea = styled.textarea<{ $error: boolean }>`
+  display: block;
+  width: 100%;
+  background-color: #161616;
+  border: none;
+  border-bottom: 1px solid ${({ $error, theme }) => ($error ? theme.colors.error : '#777777')};
+  border-radius: 0;
+  color: #777777;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.5;
+  padding: 9px 14px;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    outline: none;
+    border: none;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
+  }
+`
+
+export const Error = styled.span`
+  color: ${({ theme }) => theme.colors.error};
+  font-size: 1.2rem;
+  margin-top: 0.4rem;
+  margin-left: 0.4rem;
+`
+
+export const ButtonWrapper = styled.div`
+  /* width: 50%; */
+`
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,163 @@
+import React from 'react'
+
+import { useFormik } from 'formik'
+import { useStaticQuery, graphql } from 'gatsby'
+import { FiSend } from 'react-icons/fi'
+import * as Yup from 'yup'
+
+import { Button } from '@components'
+
+import * as S from './Form.styles'
+
+// -------------------------------------------------------------
+
+type Data = {
+  allPages: {
+    edges: {
+      node: {
+        contact_section: {
+          form: {
+            title: string
+            subtitle: string
+            button_lable: string
+          }
+        }
+      }
+    }[]
+  }
+}
+
+type FormValues = {
+  name: string
+  email: string
+  subject: string
+  message: string
+}
+
+// -------------------------------------------------------------
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  subject: Yup.string().required('Subject is required'),
+  message: Yup.string().required('Message is required'),
+})
+
+// -------------------------------------------------------------
+
+export const Form = () => {
+  const data: Data = useStaticQuery(graphql`
+    query {
+      allPages(filter: { page_id: { eq: "contact-page" } }) {
+        edges {
+          node {
+            contact_section {
+              form {
+                title
+                subtitle
+                button_lable
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const { title, subtitle, button_lable } = data.allPages.edges[0].node.contact_section.form
+
+  // FORMIK
+  const initialValues: FormValues = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+
+  const handleSubmit = (values: FormValues) => {
+    console.log(values)
+  }
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+    validateOnChange: false,
+    validateOnBlur: false,
+  })
+
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <S.Form onSubmit={formik.handleSubmit}>
+        {/* NAME */}
+        <S.InputContainer>
+          <S.Input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+            $error={!!formik.touched.name && !!formik.errors.name}
+          />
+          {formik.touched.name && formik.errors.name ? <S.Error>{formik.errors.name}</S.Error> : null}
+        </S.InputContainer>
+
+        {/* EMAIL */}
+        <S.InputContainer>
+          <S.Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            $error={!!formik.touched.email && !!formik.errors.email}
+          />
+          {formik.touched.email && formik.errors.email ? <S.Error>{formik.errors.email}</S.Error> : null}
+        </S.InputContainer>
+
+        {/* SUBJECT */}
+        <S.InputContainer>
+          <S.Input
+            id="subject"
+            name="subject"
+            type="text"
+            placeholder="Subject"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.subject}
+            $error={!!formik.touched.subject && !!formik.errors.subject}
+          />
+          {formik.touched.subject && formik.errors.subject ? <S.Error>{formik.errors.subject}</S.Error> : null}
+        </S.InputContainer>
+
+        {/* MESSAGE */}
+        <S.InputContainer>
+          <S.TextArea
+            id="message"
+            name="message"
+            placeholder="Message"
+            rows={5}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.message}
+            $error={!!formik.touched.message && !!formik.errors.message}
+          />
+          {formik.touched.message && formik.errors.message ? <S.Error>{formik.errors.message}</S.Error> : null}
+        </S.InputContainer>
+
+        {/* SUBMIT */}
+
+        <S.ButtonWrapper>
+          <Button variant="solid" as="button" type="submit" disabled={formik.isSubmitting} rightIcon={<FiSend />}>
+            {button_lable}
+          </Button>
+        </S.ButtonWrapper>
+      </S.Form>
+    </S.Container>
+  )
+}
