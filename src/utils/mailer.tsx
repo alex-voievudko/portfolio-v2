99c0c20736517a1ad34ea1eb1803ee3e6e239e1d
Alex Voievudko <oleksandrv@zeptolab.com>
2023-05-12 19:02:56 +0200

    [Utils] Set up basic axios config and send mail function

---
+++ src/utils/mailer.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+// -------------------------------------------------------------
+
+type MailPayload = {
+  securityCode?: string
+  name: string
+  email: string
+  subject: string
+  message: string
+}
+
+type MailResponse = {
+  success: boolean
+  message: string
+}
+
+// -------------------------------------------------------------
+
+const instance = axios.create({
+  baseURL: process.env.GATSBY_MAIL_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export const sendMail = async (data: MailPayload) => {
+  const response = await instance.post<MailResponse>('/api/mail', {
+    ...data,
+    securityCode: process.env.GATSBY_MAIL_API_SECURITY_CODE,
+  })
+  return response.data
+}
