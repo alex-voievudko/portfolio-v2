5813ea8fc444a132c274b4c12b6e2f9c9b5bce9d
Alex Voievudko <oleksandrv@zeptolab.com>
2023-05-21 13:53:11 +0200

    [Assets] Changed site images

--- src/sections/HeroSection/HeroSection.styles.tsx
+++ src/sections/HeroSection/HeroSection.styles.tsx
@@ -28,7 +28,7 @@ export const Background = styled(GatsbyImage)`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.2);
     z-index: 1;
   }
 
